994c404516dca4e55db2bc4e937e63b1f75176a0
Parker Brandt <parker.brandt101@gmail.com>
2024-10-12 23:06:21 -0500

    Initial LibGDX commit

---
+++ desktop/src/com/lucentus/denkimon/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.lucentus.denkimon;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.lucentus.denkimon.Denkimon;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Denkimon");
+		new Lwjgl3Application(new Denkimon(), config);
+	}
+}
