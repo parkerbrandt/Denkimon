9f042116ea84e8207c1b6cce850443a947b3255e
Parker Brandt <parker.brandt101@gmail.com>
2024-10-14 16:52:01 -0500

    Added some basic files for later Login Server implementation

---
+++ core/src/com/lucentus/denkimon/servers/LoginServer.java
@@ -0,0 +1,12 @@
+package com.lucentus.denkimon.servers;
+
+/**
+ * The Log-In Server
+ * Handles login and register requests from clients
+ */
+public class LoginServer {
+
+    public static void main(String[] args) {
+
+    }
+}
