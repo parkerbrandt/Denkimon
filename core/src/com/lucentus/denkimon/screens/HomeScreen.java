71b84756e62a5057d28e42e342c3977ee9e45926
Parker Brandt <parker.brandt101@gmail.com>
2024-10-15 13:57:07 -0500

    Created class for Home screen once logged in

---
+++ core/src/com/lucentus/denkimon/screens/HomeScreen.java
@@ -0,0 +1,46 @@
+package com.lucentus.denkimon.screens;
+
+import com.badlogic.gdx.Screen;
+import com.lucentus.denkimon.Denkimon;
+
+public class HomeScreen implements Screen {
+
+    public HomeScreen(final Denkimon game) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
