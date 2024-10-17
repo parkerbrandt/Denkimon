942a01bf5acca1d43ebbd5dc6f225439883a9fd3
Parker Brandt <parker.brandt101@gmail.com>
2024-10-17 15:34:08 -0500

    Added client-side login logic

--- core/src/com/lucentus/denkimon/screens/LoginScreen.java
+++ core/src/com/lucentus/denkimon/screens/LoginScreen.java
@@ -4,13 +4,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
 import com.lucentus.denkimon.Denkimon;
 import com.lucentus.denkimon.servers.LoginServer;
+import com.sun.corba.se.pept.encoding.OutputObject;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -32,6 +37,7 @@ public class LoginScreen implements Screen {
     private Skin skin;
     private Stage stage;
     private Table table;
+    private Table buttonTable;
 
     private Label usernameLabel;
     private TextField usernameField;
@@ -53,38 +59,40 @@ public class LoginScreen implements Screen {
         Gdx.input.setInputProcessor(stage);
 
         table = new Table();
-        table.setFillParent(true);
         stage.addActor(table);
+        table.setSize(Denkimon.VIEWPORT_WIDTH, Denkimon.VIEWPORT_HEIGHT / 2.0f);
+        table.align(Align.center);
         table.setDebug(true);
 
         // Initialize skin components
-        skin = new Skin();
+        skin = new Skin(Gdx.files.internal("assets/skins/beta/uiskin.json"));
 
-        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
-        pixmap.setColor(Color.WHITE);
-        pixmap.fill();
-        skin.add("white", new Texture(pixmap));
+        // Add text boxes for username and password input
+        usernameLabel = new Label("Username: ", skin);
+        usernameField = new TextField("", skin);
 
-        skin.add("default", new BitmapFont());
+        passwordLabel = new Label("Password: ", skin);
+        passwordField = new TextField("", skin);
 
+        TextureRegion upRegion = skin.getRegion("default-slider-knob");
+        TextureRegion downRegion = skin.getRegion("default-slider-knob");
+        BitmapFont buttonFont = skin.getFont("default-font");
 
-        // TODO: Add text boxes for username and password input
-
+        table.add(usernameLabel);
+        table.add(usernameField);
+        table.add(passwordLabel);
+        table.add(passwordField);
 
         // Add buttons
-        // Create the style that will be used for both buttons
-        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
-        style.up = skin.newDrawable("white", Color.DARK_GRAY);
-        style.down = skin.newDrawable("white", Color.DARK_GRAY);
-        style.checked = skin.newDrawable("white", Color.BLUE);
-        style.over = skin.newDrawable("white", Color.LIGHT_GRAY);
-        style.font = skin.getFont("default");
-        skin.add("default", style);
+        buttonTable = new Table();
+        stage.addActor(buttonTable);
+        buttonTable.setFillParent(true);
+        buttonTable.bottom();
 
         // Log In Button
         // Will attempt to log in to the server and check that the user's information is valid
         loginButton = new TextButton("Log In", skin);
-        table.add(loginButton);
+        buttonTable.add(loginButton);
         loginButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent changeEvent, Actor actor) {
@@ -92,9 +100,32 @@ public class LoginScreen implements Screen {
                 try {
                     InetAddress host = InetAddress.getLocalHost();
                     game.loginSocket = new Socket(host.getHostName(), LoginServer.PORT);
+                    ObjectInputStream inputStream = new ObjectInputStream(game.loginSocket.getInputStream());
+                    ObjectOutputStream outputStream = new ObjectOutputStream(game.loginSocket.getOutputStream());
+
+                    // Send the username and password info to the server for validation
+                    String username = usernameField.getText();
+                    String password = passwordField.getText();
+
+                    String[] message = {"validate", username, password};
+
+                    System.out.println("Sending login request to server...");
+                    outputStream.writeObject(message);
+
+                    // Read server response
+                    String[] response = (String[]) inputStream.readObject();
+
+                    // Close all connection objects
+                    game.loginSocket.close();
+                    inputStream.close();
+                    outputStream.close();
 
                     // If the information matches up, take the user to the main home screen
-                    game.setScreen(new HomeScreen(game));
+                    if (response[0].equals("valid"))
+                        game.setScreen(new HomeScreen(game));
+                    else {
+                        throw new Exception("Invalid Login Information: " + response[1]);
+                    }
 
                 } catch(Exception e) {
                     System.out.println("Error connecting to login server occurred:\n" + e.getMessage());
@@ -105,7 +136,7 @@ public class LoginScreen implements Screen {
         // Register Button
         // Will register user's information in the server if it does not already exist
         registerButton = new TextButton("Register", skin);
-        table.add(registerButton);
+        buttonTable.add(registerButton);
         registerButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent changeEvent, Actor actor) {
@@ -138,24 +169,18 @@ public class LoginScreen implements Screen {
     }
 
     @Override
-    public void resize(int i, int i1) {
-
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
-    public void pause() {
-
-    }
+    public void pause() { }
 
     @Override
-    public void resume() {
-
-    }
+    public void resume() { }
 
     @Override
-    public void hide() {
-
-    }
+    public void hide() { }
 
     @Override
     public void dispose() {
