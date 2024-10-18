2ce245515a1c766c1f378e8fb7b0726714ef1d6a
Parker Brandt <parker.brandt101@gmail.com>
2024-10-18 14:05:32 -0500

    Added some basic code for rendering the battlefield and denkimon

--- core/src/com/lucentus/denkimon/screens/BattleScreen.java
+++ core/src/com/lucentus/denkimon/screens/BattleScreen.java
@@ -1,8 +1,52 @@
 package com.lucentus.denkimon.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.lucentus.denkimon.DenkimonGame;
+import com.lucentus.denkimon.entities.Denkimon;
+import com.lucentus.denkimon.users.Player;
 
-public class GameScreen implements Screen {
+
+/**
+ * Screen to display battles between two Players
+ * Each player can place their 5 Denkimon from their party onto the battlefield onto any grid
+ * The Denkimon will move on their own once each player is ready
+ */
+public class BattleScreen implements Screen {
+
+    // Properties
+    private final DenkimonGame game;
+
+    private OrthographicCamera camera;
+    private float stateTime;
+
+    private Player bluePlayer;
+    private Player redPlayer;
+
+    private Denkimon[][] battleField = new Denkimon[5][5];
+
+
+    /*
+     * Constructors
+     */
+
+    public BattleScreen(final DenkimonGame game) {
+        this.game = game;
+    }
+
+    public BattleScreen(final DenkimonGame game, Player bluePlayer, Player redPlayer) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, DenkimonGame.VIEWPORT_WIDTH, DenkimonGame.VIEWPORT_HEIGHT);
+    }
+
+
+    /*
+     * Override Methods
+     */
 
     @Override
     public void show() {
@@ -11,7 +55,26 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float v) {
+        ScreenUtils.clear(0, 0.2f, 0, 1);
 
+        // Increment state time
+        stateTime += Gdx.graphics.getDeltaTime();
+
+        // Update camera
+        camera.update();
+
+
+        game.batch.setProjectionMatrix(camera.combined);
+        game.batch.begin();
+
+        // Render the battlefield
+        drawBattlefield();
+
+        // TODO: Render each of the Blue Player's Denkimon
+
+        // TODO: Render each of the Red Player's Denkimon
+
+        game.batch.end();
     }
 
     @Override
@@ -38,4 +101,18 @@ public class GameScreen implements Screen {
     public void dispose() {
 
     }
+
+
+    /*
+     * Drawing methods
+     */
+
+    /**
+     * Draw a grid of squares to represent the Denkimon on the battlefield
+     */
+    private void drawBattlefield() {
+
+    }
+
+
 }
