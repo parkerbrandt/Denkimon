57dc60285ebaf36f860e5320bc0f41cbcd035b26
Parker Brandt <parker.brandt101@gmail.com>
2024-10-21 16:47:12 -0500

    Created classes for entities in game

---
+++ core/src/com/lucentus/denkimon/entities/Denkimon.java
@@ -0,0 +1,13 @@
+package com.lucentus.denkimon.entities;
+
+
+/**
+ * Class to represent each Denkimon
+ */
+public class Denkimon extends Entity {
+
+    @Override
+    public void render() {
+
+    }
+}
---
+++ core/src/com/lucentus/denkimon/entities/Entity.java
@@ -0,0 +1,26 @@
+package com.lucentus.denkimon.entities;
+
+
+/**
+ * Abstract Class all Game Entities (Denkimon) will inherit from
+ */
+public abstract class Entity {
+
+    /*
+     * Properties
+     */
+
+
+    /*
+     * Constructors
+     */
+    public Entity() {
+
+    }
+
+
+    /*
+     * Abstract Methods
+     */
+    public abstract void render();
+}
