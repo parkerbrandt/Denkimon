577c1b1fd23e11b8ceff9e85103f86684ee5f978
Parker Brandt <parker.brandt101@gmail.com>
2024-10-22 11:40:16 -0500

    Added a method to load animations from sprite sheets

--- core/src/com/lucentus/denkimon/entities/Entity.java
+++ core/src/com/lucentus/denkimon/entities/Entity.java
@@ -1,6 +1,12 @@
 package com.lucentus.denkimon.entities;
 
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+
 /**
  * Abstract Class all Game Entities (Denkimon) will inherit from
  */
@@ -23,4 +29,35 @@ public abstract class Entity {
      * Abstract Methods
      */
     public abstract void render();
+
+
+    /*
+     * Methods
+     */
+
+    /**
+     * Load an animation by separating a sprite sheet into frames
+     * @param spriteSheetFilename the file name of the sprite sheet in the assets folder
+     * @param sheetCols the number of columns the sprite sheet contains
+     * @param sheetRows the number of rows the sprite sheet contains
+     * @return the animation at 60 frames per second
+     */
+    protected Animation<TextureRegion> loadAnimation(String spriteSheetFilename, int sheetCols, int sheetRows) {
+
+        // Separate the sprite sheet into frames to create the animation
+        Texture sheet = new Texture(Gdx.files.internal(spriteSheetFilename));
+        TextureRegion[][] temp = TextureRegion.split(sheet,
+                                            sheet.getWidth() / sheetCols,
+                                            sheet.getHeight() / sheetRows);
+        TextureRegion[] frames = new TextureRegion[sheetCols * sheetRows];
+        int index = 0;
+        for (int i = 0; i < sheetRows; i++) {
+            for (int j = 0; j < sheetCols; j++) {
+                frames[index++] = temp[i][j];
+            }
+        }
+
+        // Use the sprite sheet to create the animation at 60 frames per second
+        return new Animation<TextureRegion>((float) (sheetCols * sheetRows) / 60, frames);
+    }
 }
