85a03d1dc4093bc52872417dea96bb09f45efc6d
Parker Brandt <parker.brandt101@gmail.com>
2025-01-20 19:27:26 -0600

    Created classes to contain battlefield and grid

---
+++ core/src/com/lucentus/denkimon/battle/field/Battlefield.java
@@ -0,0 +1,29 @@
+package com.lucentus.denkimon.battle.field;
+
+import java.util.ArrayList;
+
+/**
+ *
+ */
+public class Battlefield {
+
+    /*
+     * Properties
+     */
+    private ArrayList<BattlefieldSquare> bluePlayerField = new ArrayList<>();
+    private ArrayList<BattlefieldSquare> redPlayerField = new ArrayList<>();
+
+
+    /*
+     * Constructor
+     */
+    public Battlefield() {
+
+    }
+
+
+    /*
+     * Methods
+     */
+    
+}
---
+++ core/src/com/lucentus/denkimon/battle/field/BattlefieldSquare.java
@@ -0,0 +1,43 @@
+package com.lucentus.denkimon.battle.field;
+
+import com.lucentus.denkimon.entities.Denkimon;
+
+
+/**
+ *
+ */
+public class BattlefieldSquare {
+
+    /*
+     * Properties
+     */
+    private int gridX = -1;
+    private int gridY = -1;
+
+    private int xPx = 0;        // NOTE: (x, y) location determines lower-left point
+    private int yPx = 0;
+
+    private boolean blueSide;
+    private Denkimon denkimon;
+
+
+    /*
+     * Constructor
+     */
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @param blueSide
+     */
+    public BattlefieldSquare(int x, int y, boolean blueSide) {
+
+    }
+
+
+    /*
+     * Methods
+     */
+
+}
